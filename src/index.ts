1e8b4e4895083f6b3eb4389cd16ba9761f29c1ef
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-23 10:05:08 +0100

    chore: update index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { User } from "./model/User";
 
-const user = new User({ name: "max", age: 30 }).get("age");
+const user = new User({ name: "max", age: 30 });
 
-console.log(user);
+user.on("change", () => {
+  console.log("Change #1");
+});
+
+user.on("change", () => {
+  console.log("Change #2");
+});
+
+user.on("save", () => {
+  console.log("Save #1");
+});
