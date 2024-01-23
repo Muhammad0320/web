92985b69bc23156927c2e84d4303fbd989ffe289
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-23 10:11:53 +0100

    chore: update index name to main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
- 
-`;
+import { User } from "./model/User";
+
+const user = new User({ name: "max", age: 30 });
+
+user.on("change", () => {
+  console.log("Change #1");
+});
+
+user.on("change", () => {
+  console.log("Change #2");
+});
+
+user.on("save", () => {
+  console.log("Save #1");
+});
+
+user.trigger("change");
