99c5ddb86439350e51e462403b134f5e432d7bd9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-24 04:52:06 +0100

    chore: build the eventing class

--- src/model/Eventing.ts
+++ src/model/Eventing.ts
@@ -1,1 +1,23 @@
-export class Eventing {}
+type Callback = () => void;
+
+export class Eventing {
+  events: { [eventName: string]: Callback[] } = {};
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+
+    handlers.push(callback);
+
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string) {
+    const handlers = this.events[eventName];
+
+    if (!handlers || !handlers.length) {
+      return;
+    }
+
+    handlers.forEach((callback) => callback());
+  }
+}
