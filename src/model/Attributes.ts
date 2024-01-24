322aa7c9b6959d578dff33287de8c4ae74a68ec0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-24 08:45:28 +0100

    chore: update Attributes ts

--- src/model/Attributes.ts
+++ src/model/Attributes.ts
@@ -1,1 +1,11 @@
-export class Attributes {}
+export class Attributes<T extends Object> {
+  constructor(private data: T) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(newData: T) {
+    Object.assign(this.data, newData);
+  }
+}
