222380d08ba704bd0a8cf6bcba1834c8e7e1be07
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-24 07:47:53 +0100

    chore: fix the error in sync ts

--- src/model/Sync.ts
+++ src/model/Sync.ts
@@ -1,1 +1,22 @@
-export class Sync {}
+import axios, { AxiosPromise, AxiosResponse } from "axios";
+import { UserProps } from "./User";
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  async fetch(id: string): AxiosPromise {
+    const res: AxiosResponse = await axios.get(`${this.rootUrl}/${id}`);
+
+    return res.data;
+  }
+
+  save(user: UserProps): AxiosPromise {
+    const { id } = user;
+
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, user);
+    } else {
+      return axios.post(`${this.rootUrl}`, user);
+    }
+  }
+}
