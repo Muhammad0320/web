0e3559300e2d95091d7781666fbf73bd608ce1de
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-25 06:53:03 +0100

    chore: update model ts file

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,1 +1,24 @@
+import { AxiosPromise } from "axios";
+import { Callback } from "./Eventing";
+
+interface ModelAtributes<T> {
+  getAll(): T;
+
+  set(newData: T): void;
+
+  get<K extends keyof T>(key: K): T[K];
+}
+
+interface Sync<T> {
+  fetch(id: string): AxiosPromise;
+
+  save(user: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: Callback): void;
+
+  trigger(eventName: string): void;
+}
+
 class Model {}
