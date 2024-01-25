bb87e66b37140227354c82bd89a4475056cca2c7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-25 08:16:40 +0100

    chore: update collections

--- src/model/Collections.ts
+++ src/model/Collections.ts
@@ -1,6 +1,10 @@
+import axios, { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
+import { User, UserProps } from "./User";
 
 export class collections {
+  models: User[] = [];
+
   events: Eventing;
 
   constructor(public rootUrl: string) {}
@@ -11,5 +15,17 @@ export class collections {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  async fetch() {
+    const res: AxiosResponse = await axios.get(this.rootUrl);
+
+    res.data.forEach((data: UserProps) => {
+      const user = User.buildUser(data);
+
+      this.models.push(user);
+
+      this.trigger("change");
+    });
   }
 }
