0379a92e657356050718ea82d04e465ef4f5093c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-25 07:16:51 +0100

    chore:  update Model ts

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -21,4 +21,56 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-class Model {}
+interface HasId {
+  id?: string;
+}
+
+class Model<T extends HasId> {
+  constructor(
+    public attribute: ModelAtributes<T>,
+    public sync: Sync<T>,
+    public events: Events
+  ) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attribute.get;
+  }
+
+  set(data: T) {
+    this.attribute.set(data);
+
+    this.trigger("change");
+  }
+
+  async fetch() {
+    const id = this.get("id");
+
+    if (!id) {
+      throw new Error(" There is no user with this id  ");
+    }
+
+    const data = await this.sync.fetch(id);
+
+    console.log(data.data);
+
+    console.log(data);
+
+    // this.set(data.data);
+  }
+
+  async save() {
+    console.log(this.attribute.getAll());
+
+    await this.sync.save(this.attribute.getAll());
+
+    this.trigger("save");
+  }
+}
