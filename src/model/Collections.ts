1a7e9279609cd9e0b441688c96a2457c29f0cf41
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-25 08:11:25 +0100

    chore: update collections.ts file

--- src/model/Collections.ts
+++ src/model/Collections.ts
@@ -1,1 +1,15 @@
-export class collections {}
+import { Eventing } from "./Eventing";
+
+export class collections {
+  events: Eventing;
+
+  constructor(public rootUrl: string) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+}
