9c236c3116976db9b865dafa888317576b0010e8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-26 07:32:03 +0100

    chore:  update user ts file

--- src/model/User.ts
+++ src/model/User.ts
@@ -2,6 +2,7 @@ import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { ApiSync } from "./ApiSync";
 import { Model } from "./Model";
+import { Collections } from "./Collections";
 
 export interface UserProps {
   name?: string;
@@ -17,6 +18,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new ApiSync<UserProps>(rootUrl),
       new Eventing()
+    );
+  }
+
+  static buildUserCollection(): Collections<User, UserProps> {
+    return new Collections<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
