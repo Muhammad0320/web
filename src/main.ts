4be61c343a6a529100e0bdad5132d091f5e121ff
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-26 07:07:31 +0100

    chore: update mian ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { Collections } from "./model/Collections";
+import { User, UserProps } from "./model/User";
 
-const collection = new Collections("http://localhost:3000/users");
+const collection = new Collections<User, UserProps>(
+  "http://localhost:3000/users",
+  (json: UserProps) => User.buildUser(json)
+);
 
 collection.on("change", () => {
   console.log(collection);
