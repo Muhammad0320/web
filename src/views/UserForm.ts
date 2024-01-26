7f54f7925af265c1cfcbcffa82ff28815894c5a4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-26 07:57:53 +0100

    chore: update user form comp

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,3 +1,21 @@
 class USerForm {
   constructor(public parent: Element) {}
+
+  template(): string {
+    return `
+
+                <div> 
+                    <h1> User Form </h1> 
+                    
+                    <input />
+              </div>
+
+        `;
+  }
+
+  render() {
+    const templateElemnt = document.createElement("template");
+
+    this.parent.appendChild(templateElemnt.content);
+  }
 }
