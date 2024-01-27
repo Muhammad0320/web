d8434580a6b78943d9ed8ca4e7c452022bccf203
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-27 09:16:35 +0100

    chroe : update views class

--- src/views/Views.ts
+++ src/views/Views.ts
@@ -8,4 +8,28 @@ export abstract class Views {
       this.render();
     });
   }
+
+  buildEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    this.parent.innerHTML = " ";
+
+    const templateElemnt = document.createElement("template");
+
+    templateElemnt.innerHTML = this.template();
+
+    this.buildEvents(templateElemnt.content);
+
+    this.parent.appendChild(templateElemnt.content);
+  }
 }
