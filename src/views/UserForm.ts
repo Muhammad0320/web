730cded4946d5cd130ce5ab5095c5722b065d777
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-27 18:16:03 +0100

    chore: update userForm  and userShow class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,10 +5,8 @@ export class UserForm extends Views<User, UserProps> {
   template(): string {
     return `
                 <div> 
-                    <h1> User Form </h1> 
-                    <div>  User Name: ${this.model.get("name")}  </div>
-                    <div>  User age: ${this.model.get("age")}  </div>
-                    <input />
+
+                    <input placeholder="${this.model.get("name")}" />
                     <button class='set-name' > Change Name </button>
                     <button class='set-age' > Set Random Age </button>
               </div>
--- src/views/UserShow.ts
+++ src/views/UserShow.ts
@@ -1,1 +1,16 @@
-export class UserShow {}
+import { User, UserProps } from "../model/User";
+import { Views } from "./Views";
+
+export class UserShow extends Views<User, UserProps> {
+  template(): string {
+    return `
+        
+        <h1> User Details </h1> 
+        <div>  User Name: ${this.model.get("name")}  </div>
+        <div>  User age: ${this.model.get("age")}  </div>
+
+
+
+            `;
+  }
+}
